dbabe20e819e9675cd2d8eb64905fe3d47a07999
[TUF] AlexGamer29 <sun123vet@gmail.com>
2023-03-07 14:51:42 +0700

    add: create folder structure

---
+++ src/constants/image.js
@@ -0,0 +1,11 @@
+import Css from "../assets/img/css.png";
+import Url from "../assets/img/url.png";
+import Html from "../assets/img/html.png";
+import Logo from "../assets/img/logo.png";
+
+export {
+    Css,
+    Url,
+    Html,
+    Logo
+};
